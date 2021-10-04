de44ef7bf9b8423ca6147873704668f0ea1b05ff
Crowdbotics <team@crowdbotics.com>
2021-10-04 17:28:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "perfect_parcel_sort_31094",
+  url: "https://perfect_parcel_sort_31094.botics.co",
+  api: "https://perfect_parcel_sort_31094.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
